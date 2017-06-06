2c667d9928ca9c21e1405fd686e4338aa5b96ea3
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2017-06-06 15:24:34 +0000

    finished silly string lab

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,19 @@
 #code your solutions here
 
+my_word = "utopia"
+puts my_word.upcase
+
+word = "I'M NOT SHOUTING"
+puts word.downcase
+
+puts "Karlie Kloss".length
+
+puts "wow" + " mom!!"
+
+puts "i really like programming".capitalize
+
+
+
+
 
 
